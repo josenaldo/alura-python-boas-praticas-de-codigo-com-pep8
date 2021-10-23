433b491a6f70bfd4c7c8711deb74dc102fbd3bfb
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-10-23 14:13:18 -0300

    Curso Python: Boas práticas de código com PEP8 Aula 01 - Criação da classe filanormal

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fila_normal import filanormal
+
+fila_teste = filanormal()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+
+print(fila_teste.chamacliente(5))
+print(fila_teste.chamacliente(6))
+print(fila_teste.chamacliente(6))
+print(fila_teste.chamacliente(6))
+print(fila_teste.chamacliente(6))
